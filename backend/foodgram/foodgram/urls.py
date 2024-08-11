60be81566c7c6b805a8df94e6c8be5eec2d11a71
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2024-08-11 23:01:24 +0700

    pre

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
-from django_short_url import views as surl_views
 from django.urls import include, path
 
 
