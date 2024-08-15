6d59d6d01c5aeef7c13bcae1e7dbe60aa2eed920
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2024-08-15 22:44:59 +0700

    go

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,6 @@
 # Thirdparty imports
 from django.contrib.auth import get_user_model
 from django.db import transaction
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
